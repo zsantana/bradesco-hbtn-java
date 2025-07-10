212279e23d0bad9e672261206e07563fdfdc81fa
zsantana <reinaldojsantana@gmail.com>
2025-07-10 19:09:58 +0000

    Implementando retangulo completo

---
+++ Heranca/retangulo_completo/Retangulo.java
@@ -0,0 +1,34 @@
+public class Retangulo extends FormaGeometrica{
+
+    private double altura;
+    private double largura;
+
+    @Override
+    public double area(){
+       return this.altura * this.largura;
+    };
+
+    public String toString(){
+        return String.format("[Retangulo] %.2f / %.2f", this.altura, this.largura);
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+
+        if (altura < 0 ) throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+
+        this.altura = altura;
+    }
+    public double getLargura() {
+        return largura;
+    }
+    public void setLargura(double largura) {
+
+        if (largura < 0 ) throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+
+        this.largura = largura;
+    }
+    
+}
