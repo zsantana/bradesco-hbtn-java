c98bf07a4a508f35dbe3b520883fef01f27e350c
zsantana <reinaldojsantana@gmail.com>
2025-07-10 19:28:40 +0000

    Implementando classe abstrata

---
+++ Heranca/abstrata/Retangulo.java
@@ -0,0 +1,34 @@
+public class Retangulo extends FormaGeometrica{
+
+    protected double altura;
+    protected double largura;
+
+    @Override
+    public double area(){
+       return this.altura * this.largura;
+    };
+
+    public String toString(){
+        return String.format("[Retangulo] %.2f / %.2f", this.largura, this.altura);
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+
+        if (altura < 0 ) throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+
+        this.altura = altura;
+    }
+    public double getLargura() {
+        return largura;
+    }
+    public void setLargura(double largura) {
+
+        if (largura < 0 ) throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+
+        this.largura = largura;
+    }
+    
+}
