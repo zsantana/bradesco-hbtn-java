c5aba60206e98391da3be8dfaf05bb9fdd884e27
zsantana <reinaldojsantana@gmail.com>
2025-07-10 11:18:39 +0000

    AJuste nos atributos

--- Classes_Objetos/personagem/PersonagemGame.java
+++ Classes_Objetos/personagem/PersonagemGame.java
@@ -1,20 +1,9 @@
 
 public class PersonagemGame {
         
-        private int saudeAtual;
-        private String nome;
+        public int saudeAtual;
+        public String nome;
 
-        public int getSaudeAtual() {
-                return saudeAtual;
-        }
-        public void setSaudeAtual(int saudeAtual) {
-                this.saudeAtual = saudeAtual;
-        }
-        public String getNome() {
-                return nome;
-        }
-        public void setNome(String nome) {
-                this.nome = nome;
-        }
+       
 }
     
