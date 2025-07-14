9f7b1d17eff84d99553842d822a1b340eda066c2
zsantana <reinaldojsantana@gmail.com>
2025-07-14 13:32:26 +0000

    Ajuste no log de saída do email

--- Interfaces/workflow/Email.java
+++ Interfaces/workflow/Email.java
@@ -1,8 +1,8 @@
 public class Email implements CanalNotificacao{
 
     public void notificar(Mensagem mensagem){
-        System.out.println("[EMAIL] " + 
-        mensagem.getTipoMensagem() + " - " +
+        System.out.println("[EMAIL] {" + 
+        mensagem.getTipoMensagem() + "} - " +
         mensagem.getTexto());
     }
     
