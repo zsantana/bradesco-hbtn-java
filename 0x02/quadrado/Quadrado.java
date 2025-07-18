2d5e433f6b4e20a15c244835ab6c0ae315878354
zsantana <reinaldojsantana@gmail.com>
2025-07-18 12:30:37 +0000

    Imp Quadrado

---
+++ 0x02/quadrado/Quadrado.java
@@ -0,0 +1,8 @@
+public class Quadrado {
+    public static double area(double lado) {
+        if (lado <= 0) {
+            throw new IllegalArgumentException("Lado deve possuir valor positivo");
+        }
+        return lado * lado;
+    }
+}
