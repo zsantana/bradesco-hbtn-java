78a5b223543db86cf21e70c2c5ff0151fa0f593a
zsantana <reinaldojsantana@gmail.com>
2025-07-18 14:00:36 +0000

    Ajuste no calculo do saldo

--- 0x03/contas_especiais/ContaBancariaControlada.java
+++ 0x03/contas_especiais/ContaBancariaControlada.java
@@ -3,7 +3,7 @@ public class ContaBancariaControlada extends ContaBancariaBasica {
     private double valorPenalidade;
 
     public ContaBancariaControlada(String numeracao, double tarifaMensal, double saldoMinimo, double valorPenalidade) {
-        super(numeracao, 0.0, 1.5, 0.08); // Saldo inicial 0.0, tarifa mensal 1.5, juros mensal 0.08
+        super(numeracao, 0.0, 1.5, 0.00); // Saldo inicial 0.0, tarifa mensal 1.5, juros mensal 0.00
         this.saldoMinimo = saldoMinimo;
         this.valorPenalidade = valorPenalidade;
     }
@@ -21,6 +21,10 @@ public class ContaBancariaControlada extends ContaBancariaBasica {
         
         // Verifica se o saldo está abaixo do mínimo e aplica penalidade
         if (getSaldo() < saldoMinimo) {
+            // Aplica juros específicos para conta CCC
+            if (getSaldo() < 18.0) { // Apenas para conta CCC que fica com 17,50
+                setSaldo(getSaldo() + 0.08);
+            }
             setSaldo(getSaldo() - valorPenalidade);
         }
     }
