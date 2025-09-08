a69c6448fefc02d9c4b7b1d72d8b5101dbf8167d
zsantana <reinaldojsantana@gmail.com>
2025-09-08 11:43:03 +0000

    impl collection 2

---
+++ 0x19/idade_por_cargo/Pessoa.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+
+public class Pessoa {
+    private int id;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int id, String nome, String cargo, int idade, double salario) {
+        this.id = id;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public String getCargo() { return cargo; }
+    public int getIdade() { return idade; }
+    public double getSalario() { return salario; }
+
+    @Override
+    public String toString() {
+        // Formata o salário com 6 casas decimais e vírgula
+        return String.format(Locale.US, "[%d] %s %s %d R$ %.6f", id, nome, cargo, idade, salario).replace('.', ',');
+    }
+}
