4edfc2a883da59a756163b62cda17df4390c5ba4
zsantana <reinaldojsantana@gmail.com>
2025-09-29 12:05:53 +0000

    Impl collection II

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo_idade/Pessoa.java
@@ -0,0 +1,36 @@
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+    // getters
+    public String getCargo() { return cargo; }
+    public int getIdade() { return idade; }
+}
