17745e17a4c2a6978731422a1be80980c4e95d73
zsantana <reinaldojsantana@gmail.com>
2025-09-29 11:21:05 +0000

    update

--- Streams_Collectors_Optional/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
+++ Streams_Collectors_Optional/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.util.stream.*;
 
 public class ConsultaPessoas {
-    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+    public static TreeMap<String, LinkedHashSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
         return pessoas.stream()
             .collect(Collectors.groupingBy(
                 Pessoa::getCargo,
                 () -> new TreeMap<>(Comparator.reverseOrder()),
-                Collectors.toCollection(TreeSet::new)
+                Collectors.toCollection(LinkedHashSet::new)
             ));
     }
 }
