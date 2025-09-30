e7ce1918559f7f2133de1f58bbfb608a83b6436d
zsantana <reinaldojsantana@gmail.com>
2025-09-30 11:34:03 +0000

    ajuste

--- threads/2/Produtor.java
+++ threads/2/Produtor.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class Produtor extends Thread {
     private final Fila fila;
-    private final Random rand = new Random();
+    private final Random random = new Random();
 
     public Produtor(Fila fila) {
         this.fila = fila;
@@ -13,7 +13,7 @@ public class Produtor extends Thread {
     public void run() {
         try {
             while (true) {
-                int numero = rand.nextInt(100) + 1;
+                int numero = random.nextInt(100) + 1;
                 fila.adicionar(numero);
                 Thread.sleep(1000); // produz um item a cada 1 segundo
             }
